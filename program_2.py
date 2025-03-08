e6a6bdbcb5c47e9debbfcc50396c9588afbcf14b
J-pixel440 <Joshplath1@gmail.com>
2025-03-07 22:53:46 -0600

    Update program_2.py
    By Josh Plath, 3.7.2025

--- program_2.py
+++ program_2.py
@@ -28,7 +28,9 @@ def main():
 # a list, and a number. The function displays all of the numbers
 # in the list that are greater than the number.
 def display_larger_than_n_list(n, n_list):
-    # Write your code to display all of the numbers in the list that are greater than then number n. below
+    for num in n_list:
+             if num > n:
+                print(num, end=' ')  
     print('In display_larger_than_n_list')
         
 # Call the main function.
